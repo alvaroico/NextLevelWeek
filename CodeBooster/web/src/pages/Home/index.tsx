1790e39dc2dd4bdb4e43f6c54d2bb5672ed27429
Álvaro Ribeiro Pereira <59286021+alvaroico@users.noreply.github.com>
2020-06-04 00:48:36 -0300

    npm install react-icons

---
+++ CodeBooster/web/src/pages/Home/index.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+import './styles.css';
+import logo from '../../assets/logo.svg';
+
+
+const home = () => {
+  return (
+    <div id="page-home">
+      <div className="content">
+        <header>
+          <img src={logo} alt="Ecoleta"/>
+        </header>
+
+        <main>
+          <h1>Seu marketplace de coleta de resíduos.</h1>
+          <p>Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.</p>
+
+          <a href="/cadastro">
+            <span>
+              >
+            </span>
+            <strong>
+            Pesquisar pontos de coleta
+            </strong>
+            </a>
+        
+        
+        
+        
+        
+        
+        </main>
+      </div>
+    </div>
+  )
+}
+
+export default home;
