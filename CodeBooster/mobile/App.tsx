0c02abc67eb2f26c53f3e8a336a5cb1ed2e11e60
Álvaro Ribeiro Pereira <59286021+alvaroico@users.noreply.github.com>
2020-06-05 14:16:00 -0300

    Restart Mobile

--- CodeBooster/mobile/App.tsx
+++ CodeBooster/mobile/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StatusBar } from 'react-native';
 
-import Home from './src/Home'
+import Home from './src/pages/Home'
 
 export default function App() {
   return (
---
+++ CodeBooster/mobile/src/pages/Home/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Image } from "react-native";
+
+const Home = () => {
+    return (
+        <View> 
+            <Image source={require('E:\Meus Documentos\Aulas Rocketseat\NextLevelWeek\CodeBooster\mobile\src\assets\logo.png')} />
+        </View>
+    );
+}; 
+
+export default Home;
